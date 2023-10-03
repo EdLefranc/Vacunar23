427df834009d0f26f329e3e9269d819969432cec
Edu-Lefranc <105300446+EdLefranc@users.noreply.github.com>
2023-10-03 17:40:18 -0300

    Actualización de clase CiudadanoData
    Métodos guardar y buscar ciudadano terminados.

--- VacunAR23/src/vacunar23/VacunAR23.java
+++ VacunAR23/src/vacunar23/VacunAR23.java
@@ -5,7 +5,9 @@
  */
 package vacunar23;
 
+import AccesoADatos.CiudadanoData;
 import AccesoADatos.Conexion;
+import Entidades.Ciudadano;
 
 /**
  *
@@ -17,10 +19,14 @@ public class VacunAR23 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Conexion conex = new Conexion();
+        //Conexion conex = new Conexion();
         
-        conex.Conexion_Maria();
+        Ciudadano citizen = new Ciudadano(42266409, "Mariana", "Delfos", "marianadelfos@gmail.com", "150596690", "Covid", 25, "Mariana Delfos");
+        CiudadanoData ciuData = new CiudadanoData();
         
+        //conex.Conexion_Maria();
+        //ciuData.guardarCiudadano(citizen);
+        ciuData.buscarCiudadanoPorDni(39266240);
     }
     
 }
